243150d027ba6056fa72824e98d0edde4c8694c2
David Gardenier <davidgardenier@gmail.com>
2017-03-10 16:47:29 +0100

    Setting up auto testing with Travis

---
+++ test/test.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('../code/')
+
+from populate import generate
+from dosurvey import observe
+from plot import plot_pop
+from log import pprint
+
+# Generate FRB population
+population = generate(10)
+
+# Observe FRB population
+survey_population = observe(population, 'WHOLESKY')
