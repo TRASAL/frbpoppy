375023630b4e11167cb66f69cc8367e5f698431b
David Gardenier <davidgardenier@gmail.com>
2018-05-23 11:35:41 +0200

    Updating beam pattern calculations

---
+++ tests/obs_deep_shallow.py
@@ -0,0 +1,38 @@
+"""Compare detections a deep and shallow survey."""
+from frbpoppy.do_populate import generate
+from frbpoppy.do_survey import observe
+from frbpoppy.do_plot import plot
+from frbpoppy.population import unpickle
+
+MAKE = True
+
+if MAKE:
+    days = 7
+    n_per_day = 5000
+
+    # Generate FRB population
+    population = generate(n_per_day*days,
+                          days=days,
+                          lum_dist_pars=[1e40, 1e45, 0.],
+                          z_max=2.5,
+                          pulse=[0.1, 10],
+                          si_pars=[0., 0.],
+                          repeat=0.0)
+
+    # Observe FRB population
+    deep = observe(population, 'TEST-DEEP', gain_pattern='perfect')
+    deep.name = 'deep'
+    deep.pickle_pop()
+
+    # Observe FRB population
+    shallow = observe(population, 'TEST-SHALLOW', gain_pattern='perfect')
+    shallow.name = 'shallow'
+    shallow.pickle_pop()
+
+
+else:
+    deep = unpickle('deep')
+    shallow = unpickle('shallow')
+
+# Plot populations
+plot(deep, shallow, mute=False, frbcat=False)
