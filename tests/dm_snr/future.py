bf75bed12938d9b03c2c0862249b9cfc231550dc
David Gardenier <davidgardenier@gmail.com>
2020-11-30 09:09:30 +0100

    Updating future calculations

--- tests/dm_snr/future.py
+++ tests/dm_snr/future.py
@@ -1,9 +1,12 @@
 """Check the log N log F slope for future surveys."""
 import numpy as np
 import matplotlib.pyplot as plt
+from copy import copy
 
-from frbpoppy import CosmicPopulation, Survey, SurveyPopulation, hist
+from frbpoppy import CosmicPopulation, Survey, LargePopulation, SurveyPopulation, hist
 from frbpoppy import unpickle, pprint
+import frbpoppy.direction_dists as did
+import frbpoppy.galacticops as go
 
 from tests.convenience import plot_aa_style, rel_path
 
@@ -17,27 +20,101 @@ SURVEYS = ('parkes-htru',
            'chord',
            'ska1-low',
            'ska1-mid')
-SIZE = 5e7
+SIZE = 5e4
 
 
 if MAKE:
+    # Calculate the fraction of the sky that the survey covers
+    surv_f_area = {}
+    for name in SURVEYS:
+        pop = CosmicPopulation.simple(5e5)
+        pop.gen_direction()
+        survey = Survey(name)
+        mask = survey.in_region(pop.frbs.ra, pop.frbs.dec,
+                                pop.frbs.gl, pop.frbs.gb)
+        in_surv_region = np.sum(mask)
+        tot_region = len(mask)
+
+        area_sky = 4*np.pi*(180/np.pi)**2   # In sq. degrees
+        f_area = (survey.beam_size/area_sky)*(tot_region/in_surv_region)
+
+        surv_f_area[name] = f_area
+        print(f'{name} covers {f_area*100}% of the sky')
+
     surv_pops = []
-    pop = CosmicPopulation.optimal(SIZE, generate=False)
-    pop.generate()
 
     for name in SURVEYS:
+        # Set up survey
         survey = Survey(name)
-
         if name in ('parkes-htru', 'wsrt-apertif'):
             survey.set_beam(model=name)
 
-        surv_pop = SurveyPopulation(pop, survey)
-        surv_pop.save()
+        # Set up CosmicPopulation
+        pop = CosmicPopulation.optimal(SIZE, generate=False)
+
+        # Only generate FRBs in the survey region
+        pop.set_direction(model='uniform',
+                          min_ra=survey.ra_min,
+                          max_ra=survey.ra_max,
+                          min_dec=survey.dec_min,
+                          max_dec=survey.dec_max)
+
+        # Parkes also has galactic limits:
+        if name == 'parkes-htru':
+            pop.gen_index()
+            pop.gen_dist()
+            pop.gen_time()
+
+            # Generate FRBs just within the galactic constraints
+            pop.gen_direction()
+
+            # Gather ra, dec coordinate limits
+            lims = {'min_ra': survey.ra_min, 'max_ra': survey.ra_max,
+                    'min_dec': survey.dec_min, 'max_dec': survey.dec_max}
+
+            def sample(n_gen):
+                ra, dec = did.uniform(n_srcs=n_gen, **lims)
+                gl, gb = go.radec_to_lb(ra, dec, frac=True)
+                coords = [ra, dec, gl, gb]
+                return coords
+
+            def accept(coords):
+                return survey.in_region(*coords)
+
+            coords = sample(int(SIZE))
+            mask = accept(coords)
+            reject, = np.where(~mask)
+            while reject.size > 0:
+                fill = sample(reject.size)
+                mask = accept(fill)
+                for i in range(len(coords)):
+                    coords[i][reject[mask]] = fill[i][mask]
+                reject = reject[~mask]
+
+            # Assign the values
+            frbs = pop.frbs
+            frbs.ra, frbs.dec = coords[0], coords[1]
+            frbs.gl, frbs.gb = coords[2], coords[3]
+
+            # Continue with generation
+            pop.gen_gal_coords()
+            pop.gen_dm()
+            pop.gen_w()
+            pop.gen_lum()
+            pop.gen_si()
+        else:
+            pop.generate()
+
+        surv_pop = SurveyPopulation(pop, survey, scale_by_area=False)
+        surv_pop.source_rate.f_area = surv_f_area[name]
+        surv_pop.source_rate.scale_by_area()
+        # surv_pop.save()
         surv_pops.append(surv_pop)
+
 else:
     surv_pops = []
     for name in SURVEYS:
-        surv_pops.append(unpickle(f'complex_{name}'))
+        surv_pops.append(unpickle(f'optimal_{name}'))
 
 # Start plot
 plot_aa_style(cols=2)
@@ -102,6 +179,7 @@ for i, surv_pop in enumerate(surv_pops):
     ax.set_xscale('log')
     ax.set_xlabel(r'Rate (day$^{-1}$)')
     rate = surv_pop.source_rate.det/norm_rate
+    print(f'rate: {rate}')
     line = ax.errorbar(rate, y,
                        fmt='x',
                        label=rf'{name}')
